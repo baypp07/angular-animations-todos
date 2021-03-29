c358265b6c53a0fa90d149450af8f6f70b4c390b
baypp07 <pphouone@gmail.com>
2021-03-29 18:18:50 +0200

    1 file modified

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -18,6 +18,18 @@ import {
       transition(":enter, :leave", [animate(1000)])
     ])
   ]
+  // la meme chose que :
+  // animations: [
+  //   trigger("fade", [
+  //     state('void', style({ opacity:0})),
+  //     transition("void => *", [
+  //       style({ backgroundColor: "yellow", opacity: 0 }),
+  //       animate(2000, style({ backgroundColor: "white", opacity: 1 }))
+  //     ]),
+
+  //     transition("* => void", [animate(2000, style({ opacity: 0 }))])
+  //   ])
+  // ]
 })
 export class TodosComponent {
   items: any[] = [
