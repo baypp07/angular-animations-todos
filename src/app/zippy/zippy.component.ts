e60dc67319642a79e470ef561da11109a519cd99
baypp07 <pphouone@gmail.com>
2021-04-02 20:13:04 +0200

    4 files modified

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,9 +1,29 @@
 import { Component, Input } from "@angular/core";
-
+import {
+  trigger,transition,state,style,animate,useAnimation,query,animateChild, group, stagger
+} from "@angular/animations";
 @Component({
   selector: "zippy",
   templateUrl: "./zippy.component.html",
-  styleUrls: ["./zippy.component.css"]
+  styleUrls: ["./zippy.component.css"],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height:0,
+        paddingTop:0,
+        paddingBottom:0,
+        overflow:'hidden'
+      })),
+
+      transition('collapsed => expanded',[
+        animate('300ms ease-out')
+      ]),
+      transition('expanded => collapsed',[
+        animate('300ms ease-in')
+      ])
+
+    ])
+  ]
 })
 export class ZippyComponent {
   @Input("title") title: string;
